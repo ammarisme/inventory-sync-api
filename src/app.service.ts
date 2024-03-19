6c1f428ab0ecaeb4c1b8e397096aacfbfd081418
ammarisme <a.b.ameerdeen@gmail.com>
2024-03-19 15:20:41 +0530

    updated mongodb url

--- src/app.service.ts
+++ src/app.service.ts
@@ -32,7 +32,7 @@ export class AppService {
 
 async function getCollection(collection_name) {
   try {
-    const client = await MongoClient.connect('mongodb://localhost:27017/');
+    const client = await MongoClient.connect('mongodb://ec2-13-234-20-8.ap-south-1.compute.amazonaws.com:27017/');
     const db = client.db('catlitter'); // Assign connection to db after successful connection
 
     const docs = await db.collection(collection_name).find({}).toArray();
@@ -45,7 +45,7 @@ async function getCollection(collection_name) {
 
 async function getItem(collection_name, id) {
   try {
-    const client = await MongoClient.connect('mongodb://localhost:27017/');
+    const client = await MongoClient.connect('mongodb://ec2-13-234-20-8.ap-south-1.compute.amazonaws.com:27017/');
     const db = client.db('catlitter'); // Assign connection to db after successful connection
 
     const docs = await db.collection(collection_name).find({run_id : id}).toArray();
