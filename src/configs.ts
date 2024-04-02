85b2c6eb35f2014ec6268ba37f445ae91079146f
ammarisme <a.b.ameerdeen@gmail.com>
2024-04-03 00:24:00 +0530

    pointed to prod db

--- src/configs.ts
+++ src/configs.ts
@@ -1,5 +1,5 @@
 export let mongo_url = "mongodb://admin:sajahafeel123@erp.thesellerstack.com:27017"
 //'mongodb://admin:sajahafeel2216@erp.thesellerstack.com:27017'
 // ubuntu@ip-172-31-44-129:~$ mongosh "mongodb://admin:N3w3r@123P4l1m3tT@123@erp.thesellerstack.com:27017"^C
-export let mydb = 'development'
+export let mydb = 'catlitter'
 export let woocommerce_url = 'https://www.catlitter.lk'
