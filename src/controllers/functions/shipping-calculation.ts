98b36d8f140f3d404594a7082fb610bed26d506f
ammarisme <a.b.ameerdeen@gmail.com>
2024-04-05 10:01:22 +0530

    number rounding fix

--- src/controllers/functions/shipping-calculation.ts
+++ src/controllers/functions/shipping-calculation.ts
@@ -5,9 +5,9 @@ export default function targetFunction (parameters : any){
     //calculate cart weight
     let totalWeight = 0; //out 1
     order.line_items.map((lineItem) =>  {
-        totalWeight +=(Number(lineItem.product.weight) * lineItem.quantity); 
+        totalWeight +=parseFloat((lineItem.product.weight * lineItem.quantity).toFixed(2)); 
     });
-    totalWeight = Number(totalWeight.toFixed(0));
+    
 
     let totalBeforeDiscounts = 0; // out 2
     let totalLineDiscounts = 0 // out 3
@@ -23,10 +23,10 @@ export default function targetFunction (parameters : any){
     return {
         "input" : parameters,
         "output": {
-            "totalWeight" : totalWeight,
-            "totalBeforeDiscounts": totalBeforeDiscounts,
-            "totalLineDiscounts" : totalLineDiscounts,
-            "total" : total
+            "totalWeight" : totalWeight.toFixed(2),
+            "totalBeforeDiscounts": totalBeforeDiscounts.toFixed(2),
+            "totalLineDiscounts" : totalLineDiscounts.toFixed(2),
+            "total" : total.toFixed(2)
         }
     };
 }
