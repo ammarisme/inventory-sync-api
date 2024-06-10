6885e9738af384c636ee908fd624e8cfa1e07d08
ammarisme <a.b.ameerdeen@gmail.com>
2024-06-11 00:29:27 +0530

    update script

---
+++ update.sh
@@ -0,0 +1,6 @@
+sudo systemctl stop mongod
+sudo systemctl restart docker
+sudo docker build -t inventory-sync-api:latest .
+sudo systemctl start mongod
+sudo docker run -d -p 3001:3001 -e MY_DB_NAME=catlitter inventory-sync-api:latest
+sudo docker run -d -e API_URL=http://erp.thesellerstack.com:3001  woo-sync:latest
